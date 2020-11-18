3aa2a4bb8ba4db0c016e1771c837264891f7eb55
Min Kyung CHOI <haleychoi0918@gmail.com>
2020-11-19 07:44:09 +1100

    create Navbar and Button components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import Navbar from './components/Navbar';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello</h1>
-    </div>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path='/' />
+      </Switch>
+    </Router>
   );
 }
 
