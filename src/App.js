873ace0377b17fa994205dce09f1ee34740d3d6f
Min Kyung CHOI <haleychoi0918@gmail.com>
2020-11-22 10:57:47 +1100

    create Product, Services, SignUp pages and link them

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Navbar from './components/Navbar';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './components/pages/HomePage/Home';
+import Services from './components/pages/Services/Services';
+import Products from './components/pages/Products/Products';
+import SignUp from './components/pages/SignUp/SignUp';
 import Footer from './components/pages/Footer/Footer';
 
 
@@ -12,6 +15,9 @@ function App() {
       <Navbar />
       <Switch>
         <Route path='/' exact component={Home} />
+        <Route path='/services' exact component={Services} />
+        <Route path='/products' exact component={Products} />
+        <Route path='/sign-up' exact component={SignUp} />
       </Switch>
       <Footer />
     </Router>
